55b241e579f53b22533b4cdf30abd48dfe5a8f92
G Fisher <mr.g.fisher@googlemail.com>
2020-10-31 15:12:22 +0000

    Tweaks and tucks

--- __init__.py
+++ __init__.py
@@ -1,15 +1,28 @@
-from mycroft import MycroftSkill, intent_file_handler
+import re
+from os.path import dirname, join
 
+from adapt.intent import IntentBuilder
+from mycroft import MycroftSkill, intent_handler
+
+# TODO - Localization
 
 class DisplayStuff(MycroftSkill):
-    def __init__(self):
-        MycroftSkill.__init__(self)
+    @intent_handler(IntentBuilder("").require("Display").require("Words"))
+    def speak_back(self, message):
+        """
+            Repeat the utterance back to the user.
 
-    @intent_file_handler('stuff.display.intent')
-    def handle_stuff_display(self, message):
-        self.speak_dialog('stuff.display')
+            TODO: The method is very english centric and will need
+                  localization.
+        """
+        # Remove everything up to the speak keyword and repeat that
+        utterance = message.data.get('utterance')
+        repeat = re.sub('^.*?' + message.data['Display'], '', utterance)
+        self.speak(repeat.strip())
+
+    def stop(self):
+        pass
 
 
 def create_skill():
     return DisplayStuff()
-
