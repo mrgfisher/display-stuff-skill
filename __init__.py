934aba1dd9a3e09ecc6a0093e4fb55c29a84928e
Graham Fisher <g.fisher@shpconsultants.co.uk>
2021-01-15 18:35:23 +0000

    Bit hacky and geeky, but mycroft can now (re)set my network monitoring device that surfaces as a rainbow (via a socket message to the pub-sub hub)

--- __init__.py
+++ __init__.py
@@ -5,7 +5,9 @@ from adapt.intent import IntentBuilder
 from mycroft import MycroftSkill, intent_handler
 
 import websocket
+import socketio
 import json
+from datetime import datetime, date
 
 # TODO - Localization
 
@@ -28,8 +30,8 @@ class DisplayStuff(MycroftSkill):
 
         words_array = words.split(' ')
         json_command = None
+        state_model = None
 
-        ws = websocket.create_connection("ws://192.168.0.20:9001")
 
         speak_this = "oh dear, I did not recognise that"
 
@@ -41,6 +43,21 @@ class DisplayStuff(MycroftSkill):
             parm = "?q=" + "+".join(words_array)
             json_command = json.dumps({"action": "bbcSearch", "parameter": parm})
         
+        elif words_array[0] == 'test' and words_array[1] == 'rainbow':
+            speak_this = "rainbow is sad"
+            state_model = {'message_date': json_serial(datetime.now()), 'source': 'mycroft',
+                     'alert_type': 'failure', 'downtime': 'just a few mins',
+                     'fail_count': 1, 'host': 'masta',
+                     'description': 'simulated sad message from masta', 'additional': 'python is cool'}
+
+        elif words_array[0] == 'happy' and words_array[1] == 'rainbow':
+            speak_this = "rainbow is happy"
+            state_model = {'message_date': json_serial(datetime.now()), 'source': 'mycroft',
+                     'alert_type': 'success', 'downtime': 'just a few mins',
+                     'fail_count': 1, 'host': 'masta',
+                     'description': 'reset to happy state', 'additional': 'python is cool'}
+
+
         elif words_array[0] == 'weather':
             # todo
             speak_this = "displaying met office weather forecast"
@@ -57,6 +74,7 @@ class DisplayStuff(MycroftSkill):
 
 
         if not json_command is None:
+            ws = websocket.create_connection("ws://192.168.0.20:9001")
             ws.send(json_command)
 
             ws.send(json.dumps({"action": "tie_display_on"}))
@@ -65,8 +83,35 @@ class DisplayStuff(MycroftSkill):
 
         self.speak(speak_this)
 
+        if not state_model is None:
+            rainbow_message(state_model)
+
     def stop(self):
         pass
+
+
+
+def rainbow_message(model):
+    sio = socketio.Client()
+    sio.connect('http://192.168.0.5:9002')
+    sio.sleep(1)
+
+
+    sio.emit('raise alert', model)
+
+    sio.sleep(1)
+
+    sio.disconnect()
+
+
+
+def json_serial(obj):
+    # Thanks https://stackoverflow.com/questions/11875770/how-to-overcome-datetime-datetime-not-json-serializable/36142844#36142844
+    """JSON serializer for objects not serializable by default json code"""
+
+    if isinstance(obj, (datetime, date)):
+        return obj.isoformat()
+    raise TypeError("Type %s not serializable" % type(obj))
 
 
 def create_skill():
